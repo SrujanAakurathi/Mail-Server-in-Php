b1f619e696ed41d49f068edaa2196c99b8dbed4b
Srujan Kumar Aakurathi <srujan.neo@gmail.com>
2017-12-28 14:43:11 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,12 @@
+<h2 style="background:#99FF33;padding:5px" align="left">About us</h2>
+
+<p>Know about me at <a href="https://www.linkedin.com/in/srujanaakurathi/">Linkedin</a> </p>
+
+
+<p>My Projects at <a href="https://github.com/SrujanAakurathi">Github</a></p>
+
+
+<p>Check out my profile at <a href="https://www.hackerrank.com/SrujanAakurathi">Hackerrank</a></p>
+
+
+<p>Contact me at <a href="mailto:srujan.aakurathi@yahoo.co.in">Yahoo</a></p>
---
+++ welcome.php
@@ -0,0 +1,5 @@
+<h3>Welcome  <?php session_start();
+ 	echo @$_SESSION['sname'];
+ ?>
+ Your account has been created 
+to access your accout <a href="index.php?chk=3">Click here</a></h3>
